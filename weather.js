f0a9c5a926672b2f7554114f75e6ed51646aa012
khalid192 <karazani2003@gmail.com>
2025-10-14 23:06:52 +0100

    add photos and info

--- weather.js
+++ weather.js
@@ -1,7 +1,36 @@
-const apiKey = "3d4c2bf9a6c0fd81dde9d9f1c955e217";
 
-fetch(`https://api.openweathermap.org/data/2.5/forecast?q=AGADIR&appid=${apiKey}`)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  });
+let input=document.getElementById("search")
+let humidity=document.getElementById('humidity')
+let wind=document.getElementById('wind')
+let precipitation=document.getElementById('precipitation')
+let temp=document.getElementById('deg')
+let local=document.getElementById('city')
+const apiKey = "3d4c2bf9a6c0fd81dde9d9f1c955e217";let city='agadir'
+
+
+
+
+
+  window.Fsearch =function (){
+    city=input.value.trim();
+    if (city === "") {
+        input.placeholder='Please enter a city name!'}else{input.placeholder='Where do you want the weather ?'}
+    let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+  fetch(url)
+  .then(function (response){
+   return response.json()})
+  .then(function (data){
+    console.log(data)
+  humidity.innerText=data.main.humidity
+  wind.innerText=(data.wind.speed*3.6).toFixed(2);
+  deg.innerText=((data.main.temp)- 273.15).toFixed(0)+ " °C";
+  local.innerText=(data.sys.country)+'.'+(data.name)
+
+  if(data.rain){
+    precipitation.innerText=data.rain["1h"]
+  }else{ precipitation.innerText='0'}
+  
+  })
+  input.value=''
+   }
