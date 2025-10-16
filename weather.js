5e86df37fbb5cb5e769e7aae6835ac9948e06847
khalid192 <karazani2003@gmail.com>
2025-10-16 10:25:52 +0100

    add icon d.n

--- weather.js
+++ weather.js
@@ -5,11 +5,15 @@ let wind=document.getElementById('wind')
 let precipitation=document.getElementById('precipitation')
 let temp=document.getElementById('deg')
 let local=document.getElementById('city')
+let iconSrc=document.getElementById("iconeforecast")
+let descrept=document.getElementById('descprition')
 const apiKey = "3d4c2bf9a6c0fd81dde9d9f1c955e217";let city='agadir'
 
 
 
-
+if (iconSrc.src =='') {
+  console.log(1);
+}
 
   window.Fsearch =function (){
     city=input.value.trim();
@@ -21,16 +25,21 @@ const apiKey = "3d4c2bf9a6c0fd81dde9d9f1c955e217";let city='agadir'
   .then(function (response){
    return response.json()})
   .then(function (data){
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
     console.log(data)
   humidity.innerText=data.main.humidity
   wind.innerText=(data.wind.speed*3.6).toFixed(2);
   deg.innerText=((data.main.temp)- 273.15).toFixed(0)+ " °C";
   local.innerText=(data.sys.country)+'.'+(data.name)
-
+  console.log(data.weather[0].icon)
+  iconSrc.src=`./wethers_icone/${data.weather[0].icon}.png`
+  descrept.innerText=data.weather[0].description
   if(data.rain){
     precipitation.innerText=data.rain["1h"]
   }else{ precipitation.innerText='0'}
-  
+
+
+ 
   })
-  input.value=''
-   }
+  input.value=''}
+  
